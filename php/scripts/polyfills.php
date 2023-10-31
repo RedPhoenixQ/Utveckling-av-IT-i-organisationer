03b1041392319e4b71e7df159d2b9852908567c2
RedPhoenixQ <steve.dark.03@gmail.com>
2023-10-31 16:28:50 +0100

    Add str polyfills

---
+++ php/scripts/polyfills.php
@@ -0,0 +1,14 @@
+<?php
+// str polyfill for pre PHP 8
+function str_starts_with( $haystack, $needle ) {
+    $length = strlen( $needle );
+    return substr( $haystack, 0, $length ) === $needle;
+}
+function str_ends_with( $haystack, $needle ) {
+   $length = strlen( $needle );
+   if( !$length ) {
+       return true;
+   }
+   return substr( $haystack, -$length ) === $needle;
+}
+?>
