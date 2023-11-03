af0e5dc202d078558bb958c630c7fabd7be383fc
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-03 17:08:23 +0100

    Simple file to open db connection

--- php/scripts/db.php
+++ php/scripts/db.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=grupp3', "sqllab", "Hare#2022", [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    echo "<div>DB Error:". $e->getMessage() ."<div>";
+    http_response_code(500);
+    die();
+}
+?>
