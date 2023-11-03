9728e148093c2a2cf8da9be497987f062efeaaf8
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-03 15:58:41 +0100

    Create basic auth to connect ssn to ErpNext names

---
+++ sql/init.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS grupp3;
+
+DELIMITER //
+CREATE OR REPLACE PROCEDURE new_auth(ssn CHAR(12), pwd VARCHAR(100), name VARCHAR(140)) 
+BEGIN
+	SET @salt = HEX(RAND()*0xFFFFFFFF);
+	SET @hash = MD5(CONCAT(pwd, @salt));
+	INSERT INTO auth(ssn, pwd, salt, name) VALUES (ssn, @hash, @salt, name);
+END//
+
+CREATE OR REPLACE PROCEDURE verify_auth(ssn CHAR(12), pwd VARCHAR(100)) 
+BEGIN
+	SET @salt = (SELECT salt FROM auth WHERE auth.ssn = ssn);
+	SET @hash = MD5(CONCAT(pwd, @salt)) COLLATE utf8mb4_unicode_ci;
+	SELECT *, COUNT(*) as "verified" FROM auth WHERE auth.ssn = ssn AND auth.pwd = @hash;
+END//
+DELIMITER ;
+
+CREATE TABLE IF NOT EXISTS auth(
+    ssn CHAR(12) NOT NULL,
+    pwd CHAR(32) NOT NULL,
+    salt CHAR(8) NOT NULL,
+    name VARCHAR(140) NOT NULL,
+    PRIMARY KEY (ssn)
+);
