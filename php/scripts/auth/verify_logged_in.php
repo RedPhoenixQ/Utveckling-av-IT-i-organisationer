066216790fdd4c300cdeffb43359b7a5c7a2cd3c
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-03 17:07:18 +0100

    Add simple auth verification

---
+++ php/scripts/auth/verify_logged_in.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . "/../globals.php";
+require_once __DIR__ . "/../session.php";
+if (!$_SESSION[Session::IS_LOGGED_IN]) {
+    header("Location: " . ($redirect ?? ($base_url . "login")));
+    die();
+}
+?>
