2dc89467e663de59c88054d332cfd6123eacf16e
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-03 15:59:02 +0100

    Create global constants for $_SESSION

--- php/scripts/globals.php
+++ php/scripts/globals.php
@@ -1,5 +1,9 @@
 <?php 
 require_once(__DIR__ ."/polyfills.php");
+class Session {
+    public const IS_LOGGED_IN = "is_logged_in";
+    public const NAME = "name";
+}
 // Replace with updated url when not in testing
 $base_url = "/wwwit-utv/Grupp3/";
 ?>
