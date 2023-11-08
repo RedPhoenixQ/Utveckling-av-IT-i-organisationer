5f4b938e773365df506bc20d687d26d19bb18d47
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-08 14:02:34 +0100

    Make base_url dynamic to the ftp folder

--- php/scripts/globals.php
+++ php/scripts/globals.php
@@ -5,5 +5,5 @@ class Session {
     public const NAME = "name";
 }
 // Replace with updated url when not in testing
-$base_url = "/wwwit-utv/Grupp3";
+$base_url = substr(__DIR__, 19, strlen(__DIR__) - (19 + strlen("/scripts")));
 ?>
