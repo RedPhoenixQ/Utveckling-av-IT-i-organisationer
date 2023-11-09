5b82331fe992d67558ebf542c934718fe66015cf
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-09 11:37:21 +0100

    Create journal route

---
+++ php/minasidor/journal/index.php
@@ -0,0 +1,8 @@
+<?php require_once "../../scripts/auth/verify_logged_in.php" ?>
+<?php $title = "Journal"; require_once "../../templates/header.php" ?>
+
+<main>
+    <h1>Detta är min journal</h1>
+</main>
+
+<?php require_once "../../templates/footer.php" ?>
