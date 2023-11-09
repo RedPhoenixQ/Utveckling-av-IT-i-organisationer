30135adaa0336b423cd744b8fd5b99fa3d2a7431
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-09 11:41:17 +0100

    Create bokningar route

---
+++ php/minasidor/bokningar/index.php
@@ -0,0 +1,9 @@
+<?php require_once "../../scripts/auth/verify_logged_in.php" ?>
+<?php $title = "Bokningar"; require_once "../../templates/header.php" ?>
+
+<!-- Möjlighet att boka om och avboka besök i listan -->
+<main>
+    <h1>Detta är mina bokningar</h1>
+</main>
+
+<?php require_once "../../templates/footer.php" ?>
