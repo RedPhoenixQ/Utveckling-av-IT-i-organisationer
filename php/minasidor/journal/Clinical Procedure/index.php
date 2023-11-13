e8850ca29e4d31011dc4db7d0214e7fbfb91c4cd
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-13 13:21:29 +0100

    Create medical record list and detail view
    The detail view i vary basic and simply shows everything without styling

---
+++ php/minasidor/journal/Clinical Procedure/index.php
@@ -0,0 +1,41 @@
+<?php var_dump($_GET) ?>
+<?php require_once "../../../scripts/auth/verify_logged_in.php" ?>
+<?php require_once "../../../scripts/erp.php" ?>
+<?php require_once "../../../scripts/session.php" ?>
+<?php $title = "Clinical Procedure";
+require_once "../../../templates/header.php" ?>
+
+<main class="container">
+    <h1>Clinical Procedure</h1>
+    <?php
+    $erp = new Erp("Clinical Procedure");
+    $erp->add_filter(["Clinical Procedure", "patient", "=", $_SESSION[Session::NAME]]);
+    $encounter = $erp->read($_GET["name"])["data"];
+    ?>
+
+    <dl>
+        <?php foreach ($encounter as $col => $value) {
+            echo "<dt>$col</dt>";
+            echo "<dd>";
+            if (is_array($value)) {
+                foreach ($value as $key => $v) {
+                    if (is_array($v)) {
+                        echo "<div class='table-responsive'><table class='table'><thead><tr>";
+                        foreach ($v as $n => $fv) {
+                            echo "<th>$n</th>";
+                        }
+                        echo "</tr></thead><tbody><tr>";
+                        foreach ($v as $n => $fv) {
+                            echo "<td>$fv</td>";
+                        }
+                        echo "</tr></tbody></table></div>";
+                    }
+                }
+            } else {
+                echo "<dd>$value</dd>";
+            }
+        } ?>
+    </dl>
+</main>
+
+<?php require_once "../../../templates/footer.php" ?>
---
+++ php/minasidor/journal/Lab Test/index.php
@@ -0,0 +1,40 @@
+<?php require_once "../../../scripts/auth/verify_logged_in.php" ?>
+<?php require_once "../../../scripts/erp.php" ?>
+<?php require_once "../../../scripts/session.php" ?>
+<?php $title = "Lab Test";
+require_once "../../../templates/header.php" ?>
+
+<main class="container">
+    <h1>Lab Test</h1>
+    <?php
+    $erp = new Erp("Lab Test");
+    $erp->add_filter(["Lab Test", "patient", "=", $_SESSION[Session::NAME]]);
+    $encounter = $erp->read($_GET["name"])["data"];
+    ?>
+
+    <dl>
+        <?php foreach ($encounter as $col => $value) {
+            echo "<dt>$col</dt>";
+            echo "<dd>";
+            if (is_array($value)) {
+                foreach ($value as $key => $v) {
+                    if (is_array($v)) {
+                        echo "<div class='table-responsive'><table class='table'><thead><tr>";
+                        foreach ($v as $n => $fv) {
+                            echo "<th>$n</th>";
+                        }
+                        echo "</tr></thead><tbody><tr>";
+                        foreach ($v as $n => $fv) {
+                            echo "<td>$fv</td>";
+                        }
+                        echo "</tr></tbody></table></div>";
+                    }
+                }
+            } else {
+                echo "<dd>$value</dd>";
+            }
+        } ?>
+    </dl>
+</main>
+
+<?php require_once "../../../templates/footer.php" ?>
