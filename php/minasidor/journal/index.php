7d891f79604b9a729c04aec42044af314332b29a
Rasmus <rasmush993@gmail.com>
2023-11-13 11:57:14 +0100

    Added pantient encounter to journal

--- php/minasidor/journal/index.php
+++ php/minasidor/journal/index.php
@@ -1,8 +1,37 @@
 <?php require_once "../../scripts/auth/verify_logged_in.php" ?>
-<?php $title = "Journal"; require_once "../../templates/header.php" ?>
+<?php require_once "../../scripts/erp.php" ?>
+<?php require_once "../../scripts/session.php" ?>
+<?php $title = "Journal";
+require_once "../../templates/header.php" ?>
 
 <main>
     <h1>Detta är min journal</h1>
+    <?php
+    $erp = new Erp("Patient Encounter");
+    $erp->fields = ["title", "appointment", "appointment_type", "practitioner_name", "encounter_date", "encounter_time"];
+    $erp->add_filter(["Patient Encounter", "patient", "=", $_SESSION[Session::NAME]]);
+    $list = $erp->list()["data"];
+    ?>
+
+    <ol>
+        <?php foreach ($list as $encounter) { ?>
+            <li>
+                <details>
+                    <summary>
+                    Patient Encounters
+                    </summary>
+                <table class="table">
+                <tr>
+                    <?php foreach ($encounter as $col => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+                </tr>
+                </table>
+                </details>
+            </li>
+        <?php } ?>
+    </ol>
+
 </main>
 
 <?php require_once "../../templates/footer.php" ?>
