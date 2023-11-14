5171299d078c70e0fa435a329d28db9bdf6272ef
lerjeryd <johannes.lerjeryd@gmail.com>
2023-11-14 10:34:19 +0100

    Fixa datahämtning

--- php/minasidor/bokningar/index.php
+++ php/minasidor/bokningar/index.php
@@ -1,20 +1,20 @@
 <?php require_once "../../scripts/auth/verify_logged_in.php" ?>
 <?php require_once "../../scripts/erp.php" ?>
 <?php require_once "../../scripts/session.php" ?>
-<?php $title = "Bokningar"; require_once "../../templates/header.php" ?>
+<?php $title = "Bokningar";
+require_once "../../templates/header.php" ?>
 
 <!-- Möjlighet att boka om och avboka besök i listan -->
-<main>
+<main class="container">
     <h1>Detta är mina bokningar</h1>
 
-    <?php 
-        $erp = new Erp("Patient Appointment");
-        $erp->fields = ["name", "practitioner_name", "duration", "department", "appointment_date", "appointment_type"];
-        $erp->add_filter(["Patient Appointment", "patient", "=", $_SESSION[Session::NAME]]);
-        $erp->add_filter(["Patient Appointment", "status", "=","CLOSED"]);
+    <?php
+    $erp = new Erp("Patient Appointment");
+    $erp->fields = ["name", "practitioner_name", "duration", "department", "appointment_date", "appointment_type"];
+    $erp->add_filter(["Patient Appointment", "patient", "=", $_SESSION[Session::NAME]]);
+    $erp->add_filter(["Patient Appointment", "status", "=", "CLOSED"]);
 
-        $records = $erp->list()["data"];
-        var_dump($records);
+    $records = $erp->list()["data"];
     ?>
 
     <div class="table responsive">
