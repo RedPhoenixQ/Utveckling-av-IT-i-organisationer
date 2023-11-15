e00b961a3206a736b29ef8edfe5cd5afcd6bfc23
Rasmus <rasmush993@gmail.com>
2023-11-15 12:34:47 +0100

    Added link-buttons and a greeting to "Mina Sidor".

--- php/minasidor/index.php
+++ php/minasidor/index.php
@@ -3,17 +3,26 @@
 require_once "../scripts/erp.php";
 require_once "../scripts/session.php";
 ?>
-<?php $title = "Mina Sidor"; require_once "../templates/header.php" ?>
+<?php $title = "Mina Sidor";
+require_once "../templates/header.php" ?>
 
 <main>
-    <h1>Detta är mina sidor</h1>
-</main>
-
-<div>
+    <div class="d-grid gap-2 d-md-block">
+        <h2 class="h2">Mina sidor</h2>
+        <a class="btn btn-primary" href="<?= "$base_url/minasidor/journal/" ?>">Min journal</a>
+        <a class="btn btn-primary" href="<?= "$base_url/minasidor/bokningar/" ?>">Mina bokningar</a>
+        <a class="btn btn-primary" href="<?= "$base_url/minasidor/sokvard/" ?>">Sök vård</a>
+    </div>
     <?php
     $patient = new Erp("Patient");
-    var_dump($patient->read($_SESSION[Session::NAME]));
+    $patient->fields = ["patient_name"];
+    $patient->add_filter(["Patient", "patient_name", "=", $_SESSION[Session::NAME]]);
+    $records = $patient->list()["data"];
     ?>
-</div>
+    <h3>Välkommen <?php echo $_SESSION[Session::NAME]; ?></h3>
+
+
+    <?= var_dump($records); ?>
+</main>
 
 <?php require_once "../templates/footer.php" ?>
