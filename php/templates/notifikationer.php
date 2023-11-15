2bd46f08a8214a964ef8277a7693055377fda157
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-15 18:40:29 +0100

    Display notification from ErpNext

--- php/templates/notifikationer.php
+++ php/templates/notifikationer.php
@@ -1,8 +1,10 @@
-<?php 
-    $notifications = [
-        ["name" => "NOT-123-001", "seen" => true, "date" => time(),"content"=> "This is a test message for you", "category" => "Lab Test"],
-        ["name" => "NOT-123-002", "seen" => false, "date" => time() - 7*24*60*60,"content"=> "En ny bokning har skapats den 2023-05-03", "category" => "Encounter"],
-    ]
+<?php
+require_once __DIR__ ."/../scripts/erp.php";
+$erp_notification = new Erp("Gr3 Notification");
+$erp_notification->fields = ["name", "title", "description", "related_type", "related_name", "seen", "creation"];
+$erp_notification->limit_page_length = 5;
+$erp_notification->add_filter(["Gr3 Notification", "seen", "=", "false"]);
+$notifications = $erp_notification->list()["data"];
 ?>
 
 <button class="btn" type="button" onclick="document.getElementById('notification-dialog')?.showModal()">
@@ -10,7 +12,9 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16" aria-hidden="true">
             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
         </svg>
-        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"><?= count($notifications) ?></span>
+        <?php if (!empty($notifications)): ?>
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"><?= count($notifications) ?></span>
+        <?php endif ?>
         <span class="visually-hidden">Notifikationer</span>
     </span>
 </button>
@@ -30,18 +34,31 @@
                 <?php if (!$notification["seen"]) {
                     $unread = '<span class="text-primary"><span class="visually-hidden">Oläst.</span>
                     ●</span>';
-                } ?>
-                <li class="list-group-item list-group-item-action position-relative">
-                    <a class="stretched-link" href="<?=$base_url."/minasidor/notifikationer?name=".rawurlencode($notification["name"])?>">
-                        <span class="visually-hidden">Notifikationsdetaljer</span>
-                    </a>
+                }
+                switch ($notification["related_type"]) {
+                    case "Patient Appointment":
+                        // Link to appointment site when availible
+                    break;
+                    default:
+                    $link_to_related = "$base_url/minasidor/journal/" . rawurlencode($notification["related_type"]). "/?name=" . rawurlencode($notification["related_name"]);
+                    break;
+                }
+                ?>
+                <li class="list-group-item <?php if (!empty($link_to_related)) {
+                    echo "list-group-item-action"; } ?> position-relative">
+                    <?php if (isset($link_to_related)): ?>
+                        <a class="stretched-link" href="<?=$link_to_related?>">
+                            <span class="visually-hidden">Notifikationsdetaljer</span>
+                        </a>
+                    <?php endif ?>
                     <small>
                         <?= $unread ?? "" ?>
-                        <span><?= $notification["category"] ?></span>
+                        <span><?= $notification["related_type"] ?></span>
                         -
-                        <time datetime="<?= date("c", $notification["date"]) ?>"><?= date("Y/m/d H:i",$notification["date"]) ?></time>
+                        <time datetime="<?= $notification["creation"] ?>"><?= date("Y/m/d H:i", (new DateTime($notification["creation"]))->getTimestamp()) ?></time>
                     </small>
-                    <div><?= $notification["content"] ?></div>
+                    <div class="fs-5 fw-bold"><?= $notification["title"] ?></div>
+                    <small><?= $notification["description"] ?></small>
                 </li>
             <?php endforeach ?>
         </ul>
