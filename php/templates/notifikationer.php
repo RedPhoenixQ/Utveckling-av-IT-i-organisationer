e13fe221ee3dc1da28785ca09b1b475c77e2dd20
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-15 21:20:10 +0100

    Order notification by their creation

--- php/templates/notifikationer.php
+++ php/templates/notifikationer.php
@@ -4,6 +4,7 @@ require_once __DIR__ ."/../scripts/session.php";
 $erp_notification = new Erp("Gr3 Notification");
 $erp_notification->fields = ["name", "title", "description", "related_type", "related_name", "seen", "creation"];
 $erp_notification->add_filter(["Gr3 Notification", "patient", "=", $_SESSION[Session::NAME]]);
+$erp_notification->order_by("creation", Erp::ORDER_DESC);
 $erp_notification->limit_page_length = 5;
 // Could not return "data" if there was an error
 $notifications = $erp_notification->list()["data"];
