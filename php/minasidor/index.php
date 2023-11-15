47a9687e71fbe2b681836446151d1b2fdeaad199
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-15 22:28:08 +0100

    Change semantic elements

--- php/minasidor/index.php
+++ php/minasidor/index.php
@@ -7,12 +7,20 @@ require_once "../scripts/session.php";
 require_once "../templates/header.php" ?>
 
 <main class="container">
-    <h2 class="h2">Mina sidor</h2>
-    <div class="d-grid gap-2 d-md-block">
-        <a class="btn btn-primary" href="<?= "$base_url/minasidor/journal/" ?>">Min journal</a>
-        <a class="btn btn-primary" href="<?= "$base_url/minasidor/bokningar/" ?>">Mina bokningar</a>
-        <a class="btn btn-primary" href="<?= "$base_url/minasidor/sokvard/" ?>">Sök vård</a>
-    </div>
+    <h2>Mina sidor</h2>
+    <nav class="nav nav-pills flex-column flex-sm-row flex-wrap">
+        <?php 
+        $links = [
+            "Min journal" => "journal",
+            "Mina bokningar" => "bokningar",
+            "Sök vård" => "sokvard",
+        ];
+        foreach ($links as $title => $link): ?>
+            <a class="nav-link" href="<?= "$base_url/minasidor/$link/" ?>">
+                <?= $title ?>
+            </a>
+        <?php endforeach; ?>
+    </nav>
     <?php
     $patient = new Erp("Patient");
     $patient->fields = ["patient_name"];
