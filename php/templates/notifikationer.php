5d1d059cf43640838b26d346fab632e6e623b424
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-15 22:57:49 +0100

    Remove `main` element from notifications dialog
    There should only be one `main`  element on the page

--- php/templates/notifikationer.php
+++ php/templates/notifikationer.php
@@ -33,45 +33,43 @@ $unseen_amount = Erp::method("unseen_notifications", ["patient" => $_SESSION[Ses
             <span class="visually-hidden">Close</span>
         </button>
     </header>
-    <main>
-        <ul class="list-group">
-            <?php foreach( $notifications as $notification ): ?>
-                <?php if ($notification["seen"] == 0) {
-                    $unread = '<span class="text-primary"
-                    hx-post="' . "$base_url/api/notification.php" . '" hx-trigger="intersect once"
-                    hx-swap="none"
-                    hx-vals=\'' . json_encode(["name" => $notification["name"]]) . '\'
-                    ><span class="visually-hidden">Oläst.</span>
-                    ●</span>';
-                }
-                switch ($notification["related_type"]) {
-                    case "Patient Appointment":
-                        // Link to appointment site when availible
-                    break;
-                    default:
-                    $link_to_related = "$base_url/minasidor/journal/" . rawurlencode($notification["related_type"]). "/?name=" . rawurlencode($notification["related_name"]);
-                    break;
-                }
-                ?>
-                <li class="list-group-item <?php if (!empty($link_to_related)) {
-                    echo "list-group-item-action"; } ?> position-relative">
-                    <?php if (isset($link_to_related)): ?>
-                        <a class="stretched-link" href="<?=$link_to_related?>">
-                            <span class="visually-hidden">Notifikationsdetaljer</span>
-                        </a>
-                    <?php endif ?>
-                    <small>
-                        <?= $unread ?? "" ?>
-                        <span><?= $notification["related_type"] ?></span>
-                        -
-                        <time datetime="<?= $notification["creation"] ?>"><?= date("Y/m/d H:i", (new DateTime($notification["creation"]))->getTimestamp()) ?></time>
-                    </small>
-                    <div class="fs-5 fw-bold"><?= $notification["title"] ?></div>
-                    <small><?= $notification["description"] ?></small>
-                </li>
-            <?php endforeach ?>
-        </ul>
-    </main>
+    <ul class="list-group">
+        <?php foreach( $notifications as $notification ): ?>
+            <?php if ($notification["seen"] == 0) {
+                $unread = '<span class="text-primary"
+                hx-post="' . "$base_url/api/notification.php" . '" hx-trigger="intersect once"
+                hx-swap="none"
+                hx-vals=\'' . json_encode(["name" => $notification["name"]]) . '\'
+                ><span class="visually-hidden">Oläst.</span>
+                ●</span>';
+            }
+            switch ($notification["related_type"]) {
+                case "Patient Appointment":
+                    // Link to appointment site when availible
+                break;
+                default:
+                $link_to_related = "$base_url/minasidor/journal/" . rawurlencode($notification["related_type"]). "/?name=" . rawurlencode($notification["related_name"]);
+                break;
+            }
+            ?>
+            <li class="list-group-item <?php if (!empty($link_to_related)) {
+                echo "list-group-item-action"; } ?> position-relative">
+                <?php if (isset($link_to_related)): ?>
+                    <a class="stretched-link" href="<?=$link_to_related?>">
+                        <span class="visually-hidden">Notifikationsdetaljer</span>
+                    </a>
+                <?php endif ?>
+                <small>
+                    <?= $unread ?? "" ?>
+                    <span><?= $notification["related_type"] ?></span>
+                    -
+                    <time datetime="<?= $notification["creation"] ?>"><?= date("Y/m/d H:i", (new DateTime($notification["creation"]))->getTimestamp()) ?></time>
+                </small>
+                <div class="fs-5 fw-bold"><?= $notification["title"] ?></div>
+                <small><?= $notification["description"] ?></small>
+            </li>
+        <?php endforeach ?>
+    </ul>
     <footer class="mt-2 py-2 text-center sticky-bottom bg-dark">
         <a href="<?= "$base_url/minasidor/notification/" ?>">Alla notifikationer</a>
     </footer>
