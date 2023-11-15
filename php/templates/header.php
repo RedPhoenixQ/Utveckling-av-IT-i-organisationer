331a2b84a8b70c6fb3221d3c40772128ec8b0920
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-15 23:31:18 +0100

    Add margin bellow header

--- php/templates/header.php
+++ php/templates/header.php
@@ -13,7 +13,7 @@ require_once __DIR__ . "/../scripts/session.php";
     <title><?= $title ?? "Hem" ?></title>
 </head>
 <body class="d-flex flex-column" style="min-height: 100dvh; min-height: 100vh;" hx-boost="true">
-    <header class="sticky-top bg-dark-subtle">
+    <header class="sticky-top bg-dark-subtle mb-4">
         <nav class="navbar navbar-expand-lg navbar-light mx-4">
             <a class="navbar-brand" href="<?= $base_url ?>">Mölndals Vårdcentral</a>
             <button class="navbar-toggler" type="button" 
