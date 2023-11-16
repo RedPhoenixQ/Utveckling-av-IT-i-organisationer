79292746e999b8ab38e72319c1fcb4847be1f90e
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-16 12:10:56 +0100

    Move Session class to session.php
    When including only session.php, the class should be present

--- php/scripts/session.php
+++ php/scripts/session.php
@@ -1,4 +1,9 @@
 <?php
+class Session {
+    public const IS_LOGGED_IN = "is_logged_in";
+    public const NAME = "name";
+}
+
 if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 
 // Set eventual default values on the session here
