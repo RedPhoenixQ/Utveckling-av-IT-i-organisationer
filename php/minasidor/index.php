92821141be38501ef58011df537225d157ba5dab
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-16 16:00:07 +0100

    Fix minasidor by adding defaults
    Empty fields in ErpNext don't get sent over, so a fallback is needed

--- php/minasidor/index.php
+++ php/minasidor/index.php
@@ -28,16 +28,22 @@ $patient = Erp::read(Doc::PATIENT, $_SESSION[Session::NAME])["data"];
     <summary class="fs-5">Kontakt information</summary>
     <form action="" method="post">
         <div>
+            <label class="form-label" for="patient_mobile">
+                Mobile
+            </label>
+            <input class="form-control" type="mobile" name="mobile" id="patient_mobile" value="<?= $patient["mobile"] ?? "" ?>">
+        </div>
+        <div>
             <label class="form-label" for="patient_phone">
                 Phone
             </label>
-            <input class="form-control" type="phone" name="phone" id="patient_phone" value="<?= $patient["phone"] ?>">
+            <input class="form-control" type="phone" name="phone" id="patient_phone" value="<?= $patient["phone"] ?? ""  ?>">
         </div>
         <div>
             <label class="form-label" for="patient_email">
                 Email
             </label>
-            <input class="form-control" type="email" name="email" id="patient_email" value="<?= $patient["email"] ?>">
+            <input class="form-control" type="email" name="email" id="patient_email" value="<?= $patient["email"] ?? "" ?>">
         </div>
         <button class="mt-4 btn btn-primary" name="update_contact_info">Updatera kontaktinformation</button>
     </form>
