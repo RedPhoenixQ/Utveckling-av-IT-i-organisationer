8a5b34c821c57d748958f704f8bf20d1a6d0b986
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-17 22:40:00 +0100

    Add Appointment Request view

--- php/minasidor/bokningar/index.php
+++ php/minasidor/bokningar/index.php
@@ -1,11 +1,6 @@
 <?php require_once "../../scripts/auth/verify_logged_in.php" ?>
 <?php require_once "../../scripts/erp.php" ?>
 <?php require_once "../../scripts/session.php" ?>
-<?php $title = "Bokningar";
-require_once "../header.php" ?>
-
-<!-- Möjlighet att boka om och avboka besök i listan -->
-<h1>Detta är mina bokningar</h1>
 
 <?php
 $erp_appointment = new Erp(Doc::PATEINT_APPOINTMENT);
@@ -15,8 +10,56 @@ $erp_appointment->add_filter([Doc::PATEINT_APPOINTMENT, "status", "!=", "CLOSED"
 $erp_appointment->order_by("appointment_datetime", Erp::ORDER_ASC);
 
 $appointments = $erp_appointment->list()["data"];
+
+$erp_appointment_request = new Erp(Doc::APPOINTMENT_REQUEST);
+$erp_appointment_request->fields = ["creation", "category", "reason", "is_revisit", "wants_videocall"];
+$erp_appointment_request->add_filter([Doc::APPOINTMENT_REQUEST, "state", "=", "Pending"]);
+$erp_appointment_request->order_by("creation", Erp::ORDER_ASC);
+
+$pending_requests = $erp_appointment_request->list()["data"];
 ?>
 
+
+<?php $title = "Bokningar"; require_once "../header.php" ?>
+
+<h1>Bokningar</h1>
+
+<?php if (!empty($pending_requests)): ?>
+    <h2>Vårdförfrågningar</h2>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <th>Skapad</th>
+                <th>Kategori</th>
+                <th>Beskrivning</th>
+                <th>Återbesök</th>
+                <th>Önskat videosamtal</th>
+            </thead>
+            <tbody>
+                <?php foreach ($pending_requests as $request): ?>
+                    <tr>
+                        <td class="text-nowrap"><?= (new DateTime($request["creation"]))->format("y-m-d") ?></td>
+                        <td><?= $request["category"] ?></td>
+                        <td class="w-100">
+                            <?php  if (!empty($request["reason"])): ?>
+                                <details>
+                                    <summary>Beskrivning</summary>
+                                    <?= $request["reason"] ?>
+                                </details>
+                            <?php else: ?>
+                                <small class="opacity-75">Ingen beskrivning</small>
+                            <?php endif; ?>
+                        </td>
+                        <td><?= $request["is_revisit"] == 0 ? "Nej" : "Ja" ?></td>
+                        <td><?= $request["wants_videocall"] == 0 ? "Nej" : "Ja" ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+<?php endif; ?>
+
+<h2>Bokade tider</h2>
 <div class="table-responsive">
     <table class="table align-middle">
         <thead>
