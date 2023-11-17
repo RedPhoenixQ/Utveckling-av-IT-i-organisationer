292328709635867ac13419008a1e81b869b1efbe
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-17 12:54:55 +0100

    Make journal more readable

--- php/minasidor/journal/index.php
+++ php/minasidor/journal/index.php
@@ -12,38 +12,39 @@ $erp->add_filter([Doc::PATIENT_MEDICAL_RECORD, "patient", "=", $_SESSION[Session
 $records = $erp->list()["data"];
 ?>
 
-<table class="table">
-    <thead>
-        <th></th>
-        <th>Datum</th>
-        <th>Typ</th>
-        <th>Detaljer</th>
-        <th></th>
-    </thead>
-    <tbody>
-        <?php foreach ($records as $record) { ?>
-            <tr class="position-relative">
-                <th scope="row">
-                    <a class="stretched-link"
-                        href="<?= "$base_url/minasidor/journal/" . $record["reference_doctype"] . "?name=" . rawurlencode($record["reference_name"]) ?>"></a>
-                </th>
-                <td>
-                    <?= $record["communication_date"] ?>
-                </td>
-                <td>
-                    <?= $record["reference_doctype"] ?>
-                </td>
-                <td>
-                    <?= $record["subject"] ?>
-                </td>
-                <td>
-                    <?php if ($record["attach"]) { ?>
-                    <a class="btn btn-secondary position-relative z-2" href="<?= ERP::BASE_URL . $record["attach"] ?>" target="_blank" rel="noopener noreferrer" hx-boost="false">Attachment</a>
-                    <?php } ?>
-                </td>
-            </tr>
-        <?php } ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <th>Detaljer</th>
+            <th>Datum</th>
+            <th>Sammanfattning</th>
+            <th></th>
+        </thead>
+        <tbody>
+            <?php foreach ($records as $record) { ?>
+                <tr class="position-relative">
+                    <th scope="row">
+                        <a class="btn btn-primary"
+                            href="<?= "$base_url/minasidor/journal/" . $record["reference_doctype"] . "?name=" . rawurlencode($record["reference_name"]) ?>">Detaljer</a>
+                    </th>
+                    <td class="text-nowrap">
+                        <?= $record["communication_date"] ?>
+                    </td>
+                    <td class="w-100">
+                        <details>
+                            <summary><?= $record["reference_doctype"] ?></summary>
+                            <?= $record["subject"] ?>
+                        </details>
+                    </td>
+                    <td class="table-ste">
+                        <?php if ($record["attach"]) { ?>
+                        <a class="btn btn-secondary position-relative z-2" href="<?= ERP::BASE_URL . $record["attach"] ?>" target="_blank" rel="noopener noreferrer" hx-boost="false">Attachment</a>
+                        <?php } ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
 
 <?php require_once "../footer.php" ?>
