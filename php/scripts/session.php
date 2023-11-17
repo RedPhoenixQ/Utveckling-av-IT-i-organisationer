3925ce88c90829821f906d1f06293d517c43f926
RedPhoenixQ <steve.dark.03@gmail.com>
2023-11-17 09:38:29 +0100

    Add default theme to session

--- php/scripts/session.php
+++ php/scripts/session.php
@@ -11,6 +11,7 @@ if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 // Set eventual default values on the session here
 if (!isset($_SESSION[Session::IS_LOGGED_IN])) {
     $_SESSION[Session::IS_LOGGED_IN] = false;
+    $_SESSION[Session::THEME] = "output.css";
     $_SESSION[Session::BS_THEME_MODE] = "dark";
 }
 ?>
