fd18153cb960d5506c3644b2e0352a20b8f9bbaf
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-01 10:32:50 +0100

    Add more temporary people for the demo

--- php/omoss/index.php
+++ php/omoss/index.php
@@ -1,9 +1,35 @@
 <?php 
 $groups = [
-    "Ledningen" => [
+    "Verksamhetschef och Business Controller" => [
         [
-            "name" => "Test Testson",
-            "title" => "Manager",
+            "name" => "Anna Carlsson",
+            "title" => "Verksamhetschef",
+            "img" => null,
+        ], [
+            "name" => "Petter Petersson",
+            "title" => "Controller",
+            "img" => null,
+        ]
+    ],
+    "Läkare" => [
+        [
+            "name" => "Ingrid-Marie Svensson",
+            "title" => "Specialist i allmänmedicin",
+            "img" => null,
+        ], [
+            "name" => "Philip Ka",
+            "title" => "Leg. läkare",
+            "img" => null,
+        ]
+    ],
+    "Sjuksköterskor" => [
+        [
+            "name" => "Beata Brax",
+            "title" => "Distriktssköterska och chefssjuksköterska",
+            "img" => null,
+        ], [
+            "name" => "Linda Mark",
+            "title" => "Distriktssköterska",
             "img" => null,
         ]
     ]
@@ -16,10 +42,10 @@ $groups = [
 <?php foreach ($groups as $group_name => $people): ?>
     <section>
         <h2><?= $group_name ?></h2>
-        <div class="d-flex gap-2 flex-wrap">
+        <div class="d-flex gap-4 flex-wrap">
             <?php foreach ($people as $person): ?>
-                <div class="d-flex flex-column justify-content-center p-2 text-center">
-                    <img class="border border-dark-subtle    rounded-circle" width="200" height="200" src="<?= $person["img"] ?? "$base_url/static/img/avatar.jpg" ?>" alt="<?= $person["name"] ?>">
+                <div class="d-flex flex-column align-items-center p-2 text-center" style="max-width: 200px;">
+                    <img class="border border-dark-subtle rounded-circle" width="200" height="200" src="<?= $person["img"] ?? "$base_url/static/img/avatar.jpg" ?>" alt="<?= $person["name"] ?>">
                     <span class="fs-4"><?= $person["name"] ?></span>
                     <span class="opacity-75"><?= $person["title"] ?></span>
                 </div>
