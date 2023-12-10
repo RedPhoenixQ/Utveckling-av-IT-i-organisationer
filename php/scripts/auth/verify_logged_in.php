2789350337535d081b22814db66f9828545b0648
RedPhoenixQ <steve.dark.03@gmail.com>
2023-12-10 23:51:41 +0100

    Include redirect url when facing an auth block

--- php/scripts/auth/verify_logged_in.php
+++ php/scripts/auth/verify_logged_in.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../globals.php";
 require_once __DIR__ . "/../session.php";
 if (!$_SESSION[Session::IS_LOGGED_IN]) {
-    header("Location: " . ($redirect ?? ($base_url . "/login")));
+    header("Location: " . ($redirect ?? ($base_url . "/login?" . http_build_query(["redirect" => $_SERVER["REQUEST_URI"]]))));
     die();
 }
 ?>
